7be5c5bba13eedc092cbc9753fa4a3365759176e
kevin haggerty <kghaggerty@gmail.com>
2018-02-16 13:25:50 -0600

    added customerpayment class

---
+++ BangazonWebApp/Models/CustomerPayment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BangazonWebApp.Models
+{
+    public class CustomerPayment
+    {
+        [Key]
+        public int CustomerPaymenteId { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string AccountNumber { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
