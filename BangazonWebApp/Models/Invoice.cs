8d0e24a915cf77d88672ba210b40faae05469b3a
jennabsol@gmail.com <jennabsol@gmail.com>
2018-02-16 15:39:06 -0600

    LineItems Model done omg

--- BangazonWebApp/Models/Invoice.cs
+++ BangazonWebApp/Models/Invoice.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,23 @@ namespace BangazonWebApp.Models
 {
     public class Invoice
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime InvoiceDate { get; set; }
+
+        
+        public int? UserPaymentId { get; set; }
+        
+        public UserPayment UserPayment { get; set; }
+      
+        [Required]
+        public ApplicationUser User { get; set; }
+
+        public virtual ICollection<LineItem> LineItems { get; set; }
+
     }
 }
