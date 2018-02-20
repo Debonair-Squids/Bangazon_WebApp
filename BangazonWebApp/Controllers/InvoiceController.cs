b5f9467b938bc3f62d3e27589e2b68b11694f4d8
Tyler Bowman <bowmant388@gmail.com>
2018-02-20 11:16:45 -0600

    added logic for adding product to invoice

---
+++ BangazonWebApp/Controllers/InvoiceController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonWebApp.Data;
+using BangazonWebApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BangazonWebApp.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Invoices/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("Id,InvoiceDate,UserPaymentId")] int productId)
+        {
+
+            var user = _userManager.GetUserAsync(User);
+            var allInvoices = _context.Invoice.ToList();
+            Invoice inv = null;
+            foreach (Invoice i in allInvoices)
+            {
+                if (i.User.Equals(user) && i.UserPayment == null)
+                { 
+                    inv = i;
+                }
+            }
+
+            //If there is no open invoice in the DB Create a new invoice
+            if (inv == null)
+            {
+                
+                inv = new Invoice();
+                inv.InvoiceDate = DateTime.Now;
+                inv.UserPaymentId = null;
+                if (ModelState.IsValid)
+                {
+                    _context.Invoice.Add(inv);
+                    _context.SaveChanges();
+                    
+                }
+            }
+
+
+            //add product to invoice
+            LineItem li = new LineItem();
+            li.InvoiceId = inv.Id;
+            li.ProductId = productId;
+
+            _context.LineItem.Add(li);
+            _context.SaveChanges();
+
+
+            
+
+            
+
+
+            
+        }
+    }
+}
